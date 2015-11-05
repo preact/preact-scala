28af7fd368602445a53c4259bd4a569dc8bab4f4
Mihai Andreica <mihai.andreica@gmail.com>
2015-11-05 12:35:08 +0200

    - minor change in the status response message

--- src/main/scala/com/preact/api/model/IntegrationStatus.scala
+++ src/main/scala/com/preact/api/model/IntegrationStatus.scala
@@ -2,7 +2,7 @@ package com.preact.api.model
 
 import play.api.libs.json.Json
 
-case class IntegrationStatus(success:Boolean, messages:Seq[String])
+case class IntegrationStatus(connected:Boolean, messages:Seq[String])
 object IntegrationStatus {
   implicit val frmt = Json.format[IntegrationStatus]
 }
